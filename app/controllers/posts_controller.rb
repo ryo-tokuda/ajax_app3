a39bfa6c901977b0913c9665ce6913ae69457b17
ryo-tokuda <ryo.tokuda@di-v.co.jp>
2020-09-23 13:07:35 +0900

    Ajax Appの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
 
- def index 
-  @posts = Post.all
- end
+  def index 
+  @posts = Post.all.order(id: "DESC")
+  end
 
- def new
- end
 
- def create
+  def create
   Post.create(content: params[:content])
- end
+  redirect_to action: :index
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_093513) do
+ActiveRecord::Schema.define(version: 2020_05_08_053554) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
