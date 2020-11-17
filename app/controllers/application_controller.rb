a29d6ae3884b05e76d3f6da9073ef15656ba1bbc
ryo-tokuda <ryo.tokuda@di-v.co.jp>
2020-11-17 15:13:36 +0900

    BASIC認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user_name,password|
+    #環境変数を読み込む記述の変更
+    user_name == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+    end
+  end
 end
